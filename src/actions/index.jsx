c9e9a4b74a9689e40cb74638b009f79648cceddf
soubhagyasethy <ssethy017@gmail.com>
2023-03-22 00:25:52 +0530

    Changing state to add favorites as well

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,7 +1,18 @@
-{
-    type: 'ADD_MOVIES',
-    movies: [m1, m2, m3]
-}
-{
-    type: 'DECREASE_COUNT'
+// {
+//     type: 'ADD_MOVIES',
+//     movies: [m1, m2, m3]
+// }
+// {
+//     type: 'DECREASE_COUNT'
+// }
+
+// action types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+// action creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount() {
@@ -12,16 +13,13 @@ class App extends React.Component {
     });
     // make API call
     // dispatch action
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
-    // console.log("state", this.props.store.getState());
+    store.dispatch(addMovies(data));
+    console.log("STATE", this.props.store.getState());
   }
 
   render() {
-    const movies = this.props.store.getState();
-    console.log("RENDER");
+    const { list } = this.props.store.getState(); //{list: [], favorites: []}
+    console.log("RENDER", this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -32,7 +30,7 @@ class App extends React.Component {
           </div>
 
           <div className="list">
-            {movies.map((movie, index) => (
+            {list.map((movie, index) => (
               <MovieCard movie={movie} key={`movies-${index}`} />
             ))}
           </div>
